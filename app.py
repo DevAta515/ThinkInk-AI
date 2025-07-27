1fa8fb639f75334adac0612da92a353b6bc0b60c
DevAta515 <urrehmanata6@gmail.com>
2025-07-27 15:17:42 +0530

    added blog generation

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from src.graphs.graphbuilder import GraphBuilder
+from src.llms.groqllm import GroqLLM
+import uvicorn
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app=FastAPI()
+os.environ["LANGSMITH_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+@app.get("/")
+def get_method():
+    return {"data": "Hello"}
+
+@app.post("/blogs")
+async def create_blogs(request:Request):
+    data =await request.json()
+    topic=data.get("topic",)
+
+    llm=GroqLLM().get_llm()
+
+    graph_builder=GraphBuilder(llm)
+    if topic:
+        graph=graph_builder.setup_graph(usecase="Topic")
+        state=graph.invoke({"topic":topic})
+
+    return {"data":state}
+    
+
+if __name__=="__main__":
+    uvicorn.run("app:app",host="0.0.0.0", port=8080, reload=True)
